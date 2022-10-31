7337dfb507275eb67e894e4e1e56f33e74d5bf84
mohsin ali <110420390+ma2205505@users.noreply.github.com>
2022-10-31 21:18:48 +0500

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg">
+                <div className="container-fluid">
+                    <a className="navbar-brand" to="#"><img src='https://www.logodesign.net/images/home-page-logo-03.png' height={50} width={50} alt=''></img></a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" to="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" to="">About</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" to="">Services</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" to="">Contact</a>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" to="#">Action</a></li>
+                                    <li><a className="dropdown-item" to="#">Another action</a></li>
+                                    {/* <li><hr className="dropdown-divider"></li> */}
+                                    <li><a className="dropdown-item" to="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <form className="d-flex" role="search">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
